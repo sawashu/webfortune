c0cd97655a17bbcb184f0e05d0e4011ede38b8a9
sawashu <shunichi.sawamura216@gmail.com>
2021-05-05 09:22:21 -0500

    Does not work but let me see

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:18.04
+
+WORKDIR /app
+
+COPY app.py app.py
+COPY requirements.txt requirements.txt
+
+RUN apt-get update
+RUN apt-get install -y fortune fortunes
+RUN apt-get install -y cowsay
+RUN apt-get install -y python3.8
+RUN apt-get install -y python3-pip
+RUN pip3 install -r requirements.txt
+
+ENV PATH=$PATH:/usr/games
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+
+CMD [ "python3", "-m", "flask", "run", "--host=0.0.0.0" ]
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import subprocess
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('fortune'))
+
+@app.route('/fortune/')
+def fortune():
+    out = subprocess.check_output('fortune')
+    res = out.decode("utf-8")
+    return '<pre>'+res+'</pre>'
+
+@app.route('/cowsay/<message>/')
+def cowsay(message):
+    t = 'cowsay '+ message
+    cow = subprocess.run([t], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = cow.stdout.decode("utf-8")
+    return '<pre>'+res+'</pre>'
+
+@app.route('/cowfortune/')
+def cowfortune():
+    say = subprocess.run(['fortune'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    mi = say.stdout.decode("utf-8")
+    t = 'cowsay '+ mi
+    cow = subprocess.run([t], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = cow.stdout.decode("utf-8")
+    return '<pre>'+res+'</pre>'
+
