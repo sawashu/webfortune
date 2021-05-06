4f040ee47c4b9e4d50fc7a997adb84ca24c177d4
sawashu <shunichi.sawamura216@gmail.com>
2021-05-05 19:27:35 -0500

    I fixed it and it worked

--- app.py
+++ app.py
@@ -2,29 +2,30 @@ from flask import Flask, redirect, url_for
 import subprocess
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return redirect(url_for('fortune'))
 
 @app.route('/fortune/')
 def fortune():
-    out = subprocess.check_output('fortune')
-    res = out.decode("utf-8")
+    say = subprocess.run(['fortune'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = say.stdout.decode("utf-8")
     return '<pre>'+res+'</pre>'
 
 @app.route('/cowsay/<message>/')
 def cowsay(message):
     t = 'cowsay '+ message
     cow = subprocess.run([t], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if len(cow.stderr.decode("utf-8")) != 0:
+        return 'type valid text'
     res = cow.stdout.decode("utf-8")
     return '<pre>'+res+'</pre>'
 
 @app.route('/cowfortune/')
 def cowfortune():
     say = subprocess.run(['fortune'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    mi = say.stdout.decode("utf-8")
-    t = 'cowsay '+ mi
-    cow = subprocess.run([t], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cow = subprocess.run(['cowsay'], input=say.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res = cow.stdout.decode("utf-8")
-    return '<pre>'+res+'</pre>'
-
+    return '<pre>' + res + '</pre>'
